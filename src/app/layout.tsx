f04352083a84f7e019b7151408b377f9ea06fa5d
geuresti <54899735+geuresti@users.noreply.github.com>
2024-12-26 13:06:34 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SanityLive } from "./sanity/live";
+import { LogoutButton } from "./logout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,9 @@ export default function RootLayout({
       >
         {children}
         <SanityLive />
+
+        <LogoutButton />
+
       </body>
     </html>
   );
