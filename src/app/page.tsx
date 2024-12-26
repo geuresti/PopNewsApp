6a80277b693e642b3a9cfac45f718d31d4d7e05e
geuresti <54899735+geuresti@users.noreply.github.com>
2024-12-26 13:06:12 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,37 @@
-import Link from "next/link";
-import { defineQuery } from "next-sanity";
-import { sanityFetch } from "./sanity/live";
+"use client";
 
-const ARTICLES_QUERY = defineQuery(`*[
-  _type == "article"
-  && defined(slug.current)
-]{title, slug}|order(publishDate desc)`);
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { app } from "./firebase/firebase";
+import { useRouter } from "next/navigation";
 
-export default async function IndexPage() {
-  const { data: article } = await sanityFetch({ query: ARTICLES_QUERY });
+export default function LoginPage() {
+
+  const router = useRouter();
+
+  const handleGoogleLogin = async () => {
+    const auth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      //console.log("Logged in as:", user);
+      router.push("/articles");
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  };
 
   return (
-    <main className="flex bg-gray-100 min-h-screen flex-col p-24 gap-12">
-      <h1 className="text-4xl font-bold tracking-tighter">Articles</h1>
-      <ul className="grid grid-cols-1 gap-12 lg:grid-cols-2">
-        {article.map((article) => (
-          <li className="bg-white p-4 rounded-lg" key={article.title}>
-            <Link
-              className="hover:underline"
-              href={`/articles/${article?.slug.current}`}
-            >
-              <h2 className="text-xl font-semibold">{article?.title}</h2>
-              {article?.publishDate && (
-                <p className="text-gray-500">
-                  {new Date(article.publishDate).toLocaleDateString()}
-                </p>
-              )}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </main>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold mb-4">Login Page</h1>
+      <button
+        onClick={handleGoogleLogin}
+        className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+      >
+        Login with Google
+      </button>
+    </div>
   );
 }
+
